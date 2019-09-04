08a708b213a4568edb93460e6d0f57628fb7a5b8
Felipe Avelar <felipealpaca@gmail.com>
2019-09-04 17:27:41 -0300

    Aula 13 completa

---
+++ POO/CursoEmVideo/src/Aula13/Aula13.java
@@ -0,0 +1,21 @@
+package Aula13;
+
+public class Aula13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cachorro c = new Cachorro();
+		
+		c.reagir("Ola");
+		
+		c.reagir(11, 45);
+		c.reagir(19, 00);
+		
+		c.reagir(true);
+		c.reagir(false);
+		
+
+
+	}
+
+}
